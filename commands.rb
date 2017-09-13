d7defda4092f06feb1f0bd926bde0c0dfb5ca514
Stu Franks <stu.j.franks@gmail.com>
2017-09-13 17:59:10 +0100

    Setup command class spine

--- commands.rb
+++ commands.rb
@@ -1,5 +1,22 @@
 
 module Commands
   
-  class CommandBase
-    def 
+  class Base
+    def run(args)
+      puts 'running command'
+      args
+    end
+
+    def _generate_config
+
+    end
+
+  end
+
+  class Client < Base
+  end
+
+  class Server < Base
+  end
+
+end
