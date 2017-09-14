4032a1ff6a8ddc07a1ec3311d320184c79f50cd2
Stu Franks <stu.j.franks@gmail.com>
2017-09-14 14:02:57 +0100

    Remove config files

--- repoman.rb
+++ repoman.rb
@@ -26,30 +26,25 @@ $LOAD_PATH << File.dirname(__FILE__)
 # Requirements
 require 'option_parser'
 require 'commands'
-require 'config_file'
-
-# Load config file
-repoconfig = ConfigFile::Base.new('.repoman')
 
 # Parse arguments
 options = MainParser.parse(ARGV)
 
-# Update configuration options
-repoconfig.merge(options)
-
 # Decide on function to run
 case ARGV[0]
-when "client"
-  Commands.Client.run(repoconfig)
-when "server"
-  Commands.Server.run(repoconfig)
+when "generate"
+  Commands::Generate.run(options)
+when "mirror"
+  Commands::Mirror.run(options)
 else
   options = MainParser.parse(['--help']) 
 end
 
+=begin
 # Write out file
 if options['save']
   repoconfig.write_to_config
 else
   puts 'not_saving'
 end
+=end
